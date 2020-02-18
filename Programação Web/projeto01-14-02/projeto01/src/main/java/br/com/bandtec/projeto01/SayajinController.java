9e2975f4e44b9d2e0ec2b20f645bdac27a896ae0
MatrixEnergia <tidesenvolvimento@matrixenergia.com>
2020-02-18 14:48:25 -0300

    Exercicios do Yosh

--- Programação Web/projeto01-14-02/projeto01/src/main/java/br/com/bandtec/projeto01/SayajinController.java
+++ Programação Web/projeto01-14-02/projeto01/src/main/java/br/com/bandtec/projeto01/SayajinController.java
@@ -1,15 +1,42 @@
 package br.com.bandtec.projeto01;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sayajin")
 public class SayajinController {
+    private Double base;
 
-    private String sjj;
-    private String god;
-    private String blue;
-    private String base;
+    @GetMapping("/forca-base/{forca}")
+    public void forcaBase(@PathVariable("forca") Double forca){
+        this.base = forca;
+    }
+
+    @GetMapping("transformar/{forma}")
+    public String transformar(@PathVariable("forma") String forma){
+        Double calc;
+        if (base == null){
+            return "Defina a força base antes!";
+        }else{
+            if(forma.equals("ssj")){
+                calc = base*50;
+                return "Força atual "+calc;
+            }else if(forma.equals("god")){
+                calc = base*200;
+                return "Força atual "+calc;
+            }else if(forma.equals("blue")){
+                calc = base*500;
+                return "Força atual "+calc;
+            }else if(forma.equals("base")){
+                calc = base;
+                return "Força atual "+calc ;
+            }else{
+                return "Erro, insira uma forma válida";
+            }
+        }
+    }
 }
